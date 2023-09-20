e082f2ba9b504a347bae4ac6d8c33e9bf9dbbd8f
pablo <pablohdl@hotmail.com>
2023-09-19 22:05:31 -0300

    adicionar cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 const app = express()
 
 require('dotenv').config()
@@ -9,6 +10,8 @@ app.use(
     extended: true,
   })
 )
+
+app.use(cors());
 
 app.use(express.json())
 
